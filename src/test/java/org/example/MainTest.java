fbb281a04029726a66eb88f502ccd6a7b6a8706c
Carlos Campos <carloscampos@Carloss-MBP.lan>
2025-02-21 22:35:27 -0500

    Added JUnit test for Main

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Test
+    void testExample() {
+        int result = 2 + 2;
+        assertEquals(4, result, "2 + 2 should be 4");
+    }
+}
